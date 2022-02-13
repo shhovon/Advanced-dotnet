bfefb848bd0fdcb3792b05dee6026a5c7b3b05a9
Shovon <shovon.zzz5@gmail.com>
2022-02-13 13:16:40 +0600

    lab 3 task added

---
+++ Lab3-db/Lab3-db/Controllers/studentController.cs
@@ -0,0 +1,51 @@
+﻿using dbPractice.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace dbPractice.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(student s)
+        {
+            if(ModelState.IsValid)
+            {
+                int currentYear = DateTime.Now.Year;
+                s.age = currentYear - Int16.Parse((Request["ag"].ToString()).Substring(0, 4));
+                //s.cgpa = Request["ag"].Substring(3);
+                DotNetL3Entities1 db = new DotNetL3Entities1();
+                db.students.Add(s);
+                db.SaveChanges();
+                return RedirectToAction("showList");
+            }
+            return View();
+        }
+
+        public ActionResult showList()
+        {
+            DotNetL3Entities1 data = new DotNetL3Entities1();
+            var d = data.students.ToList();
+            return View(d);
+        }
+
+        public ActionResult Scholarship()
+        {
+            DotNetL3Entities1 data = new DotNetL3Entities1();
+            var d = (from obj in data.students
+                    where obj.cgpa > 3.75
+                    select obj).ToList();
+            return View(d);
+        }
+    }
+}
---
+++ Lab3-db/Lab3-db/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dbPractice.MvcApplication" Language="C#" %>
